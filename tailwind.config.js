3f8ea35547563dd1df979ac92ffb10ceb1e47c56
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-09-21 12:49:33 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,20 @@ module.exports = {
         './*.html',
     ],
     theme: {
-        theme: {
-            extend: {
-                fontFamily: {
-                    agrandir: ["Agrandir Regular", "sans-serif"],
-                    apoc: ["Apoc Normal", "serif"],
-                },
+        extend: {
+            fontFamily: {
+                agrandir: ["Agrandir Regular", "sans-serif"],
+                apoc: ["Apoc Normal", "serif"],
+            },
+            fontSize: {
+                'xxs': ['0.7rem', '1.35'], 
+            },
+            screens: {
+                '3xl': '2200px'
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 }
